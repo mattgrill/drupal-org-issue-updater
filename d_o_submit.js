114f4c8b14d947b5b302e7e7dd7754fdaeb9f343
Ted Bowman <ted.git@tedbow.com>
2017-08-30 14:39:08 -0400

    working but cached

--- d_o_submit.js
+++ d_o_submit.js
@@ -1,33 +1,39 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
-var readlineSync = require('readline-sync');
-
-var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('pass.json', 'utf8'));
 
 
-//var pass = readlineSync.question('May I have your pass? ');
+
+var config = require('./pass.json');
+
+
 nightmare
   .goto('https://www.drupal.org/user/login')
-  .type('form#user-login #edit-name', 'tedbow')
+  .type('form#user-login #edit-name', config.username)
   .type('form#user-login #edit-pass', config.pass)
   .click('form#user-login [type=submit]')
   .wait('#user-user-full-group-profile-main')
+  .goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&status%5B%5D=Open&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D')
   .evaluate(function () {
-    console.log(document.querySelector('#page-title').textContent);
+    var results = [];
+    var elements = jQuery('.view-project-issue-search-project-searchapi .project-issue tbody .views-field-title a');
+    elements.each(function (index, element) {
+      results.push(element.href)
+    });
+    return results;
+    //nightmare.goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D').wait('.bkdkd');
 
 
   })
-  .end()
+  //.end()
   .then(function (result) {
-    //console.log(result)
-    nightmare.goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D').wait('.bkdkd');
-    setTimeout(function() {
-      console.log('Blah blah blah blah extra-blah');
-    }, 3000);
+    console.log(result)
+    result.forEach(function (url) {
+      nightmare.goto(url);
+
+    });
   })
   .catch(function (error) {
     console.error('Search failed:', error);
   });
 
-//nightmare.goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D');
+nightmare.goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D').wait('.bkdkd');;
---
+++ fetcher.js
@@ -0,0 +1,49 @@
+const Nightmare = require('nightmare');
+const DrupalAPI = require('drupal-org-api');
+var config = require('./pass.json');
+
+const nightmare = Nightmare({ show: false });
+const drupalapi = new DrupalAPI();
+
+// Promise.all([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+Promise.all([0]
+  .map(index => drupalapi.node({
+      field_project: 3060,
+      type: 'project_issue',
+      field_issue_component: 'outside_in.module',
+      limit: 10,
+      page: index,
+    })
+      .then(results => results.list.map(result => `https://www.drupal.org/node/${result.nid}`))
+  ))
+  .then(responsePages => Array.prototype.concat(...responsePages.map(page => page)))
+  .then((urls) => {
+    /*urls = [urls.pop()]; */
+    nightmare
+      .goto('https://www.drupal.org/user/login')
+      .type('form#user-login #edit-name', config.username)
+      .type('form#user-login #edit-pass', config.pass)
+      .click('form#user-login [type=submit]')
+      .wait('#user-user-full-group-profile-main');
+    urls.reduce((accumulator, url) => (
+      accumulator.then(results => (
+        nightmare
+  /*        .goto('https://www.drupal.org/user/login')
+          .type('form#user-login #edit-name', config.username)
+          .type('form#user-login #edit-pass', config.pass)
+          .click('form#user-login [type=submit]')
+          .wait('#user-user-full-group-profile-main')*/
+          .goto(url)
+          .wait('body')
+          .select('#edit-field-issue-component-und', 'settings_tray.module')
+          .type('#edit-nodechanges-comment-comment-body-und-0-value', 'Changing to new <strong>settings_tray.module</strong> component. @drpal thanks for script help!')
+          .click('.form-actions [value=Save]')
+          .title()
+          .then((result) => {
+            console.log("changed: " + result);
+            results.push(result);
+            return results;
+          })
+      ))
+    ), Promise.resolve([])).then(console.log);
+  });
