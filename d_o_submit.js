6bdbf28ba27051b04a8efe11ee7f4aeeb65f2584
Ted Bowman <ted.git@tedbow.com>
2017-08-30 12:05:42 -0400

    init add

--- d_o_submit.js
+++ d_o_submit.js
@@ -14,17 +14,6 @@ nightmare
     console.log(document.querySelector('#page-title').textContent);
 
 
-  })
-  .end()
-  .then(function (result) {
-    //console.log(result)
-    nightmare.goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D').wait('.bkdkd');
-    setTimeout(function() {
-      console.log('Blah blah blah blah extra-blah');
-    }, 3000);
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
   });
 
 //nightmare.goto('https://www.drupal.org/project/issues/search/drupal?project_issue_followers=&version%5B%5D=8.x&component%5B%5D=outside_in.module&issue_tags_op=%3D');
